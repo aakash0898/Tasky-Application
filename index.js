d44d0963abbf30cd4bcc98b98f10da20925701de
Aakash Singh <aakashsingh080598@gmail.com>
2024-03-06 16:45:23 +0530

    cards and js added

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("connected")
+//var state = {
+//  taskList: [
+//    {
+//      imageUrl: "",
+//      taskTitle: "",
+//      taskType: "",
+//      taskDescription: ""
+//    },{
+//      imageUrl: "",
+//     taskTitle: "",
+//      taskType: "",
+//      taskDescription: ""
+//    },{
+//      imageUrl: "",
+//      taskTitle: "",
+//      taskType: "",
+//      taskDescription: ""
+//    },{
+//      imageUrl: "",
+//      taskTitle: "",
+//      taskType: "",
+//      taskDescription: ""
+//    },{
+//      imageUrl: "",
+//      taskTitle: "",
+//      taskType: "",
+//      taskDescription: ""
+//    }
+//  ]
+//}
+
+
+const state = {
+  taskList: [],
+};
+
+//DOM operations
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+console.log(taskContents);
